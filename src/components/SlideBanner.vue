2e2dcda8af9789c19fbfae97892f7c94f60f980a
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-07-05 14:06:30 +0200

    better SlideBanner

--- src/components/SlideBanner.vue
+++ src/components/SlideBanner.vue
@@ -1,6 +1,49 @@
 <script>
 export default {
   name: "SlideBanner",
+
+	data() {
+    return {
+      slideData: [
+        {
+          id: 1,
+          images: [
+            { id: 1, src: "https://picsum.photos/300/300?image=898", alt: "image" },
+            { id: 2, src: "https://picsum.photos/300/300?image=889", alt: "image" },
+            { id: 3, src: "https://picsum.photos/300/300?image=910", alt: "image" },
+						{ id: 4, src: "https://picsum.photos/300/300?image=911", alt: "image" },
+						{ id: 5, src: "https://picsum.photos/300/300?image=912", alt: "image" },
+						{ id: 6, src: "https://picsum.photos/300/300?image=913", alt: "image" },
+          ],
+        },
+        {
+          id: 2,
+          images: [
+            { id: 1, src: "https://picsum.photos/300/300?image=898", alt: "image" },
+            { id: 2, src: "https://picsum.photos/300/300?image=889", alt: "image" },
+            { id: 3, src: "https://picsum.photos/300/300?image=910", alt: "image" },
+						{ id: 4, src: "https://picsum.photos/300/300?image=911", alt: "image" },
+						{ id: 5, src: "https://picsum.photos/300/300?image=912", alt: "image" },
+						{ id: 6, src: "https://picsum.photos/300/300?image=913", alt: "image" },
+          ],
+        },
+				{
+          id: 3,
+          images: [
+            { id: 1, src: "https://picsum.photos/300/300?image=898", alt: "image" },
+            { id: 2, src: "https://picsum.photos/300/300?image=889", alt: "image" },
+            { id: 3, src: "https://picsum.photos/300/300?image=910", alt: "image" },
+						{ id: 4, src: "https://picsum.photos/300/300?image=911", alt: "image" },
+						{ id: 5, src: "https://picsum.photos/300/300?image=912", alt: "image" },
+						{ id: 6, src: "https://picsum.photos/300/300?image=913", alt: "image" },
+          ],
+        },
+       
+      ],
+    };
+  },
+
+
   mounted() {
     const columns = document.querySelectorAll('.column');
     columns.forEach(column => {
@@ -14,108 +57,44 @@ export default {
 
 
 <template>
-<header>
-	<div class="column">
-		<a href="#"><img src="https://picsum.photos/300/300?image=890" alt="image"></a>
-		<a href="#"><img src="https://picsum.photos/300/300?image=891" alt="image"></a>
-		<a href="#"><img src="https://picsum.photos/300/300?image=892" alt="image"></a>
-		<a href="#"><img src="https://picsum.photos/300/300?image=893" alt="image"></a>
-	</div>
-	<div class="column">
-		<a href="#"><img src="https://picsum.photos/300/300?image=894" alt="image"></a>
-		<a href="#"><img src="https://picsum.photos/300/300?image=885" alt="image"></a>
-		<a href="#"><img src="https://picsum.photos/300/300?image=896" alt="image"></a>
-		<a href="#"><img src="https://picsum.photos/300/300?image=887" alt="image"></a>
-	</div>
-	<div class="column">
-		<a href="#"><img src="https://picsum.photos/300/300?image=898" alt="image"></a>
-		<a href="#"><img src="https://picsum.photos/300/300?image=889" alt="image"></a>
-		<a href="#"><img src="https://picsum.photos/300/300?image=910" alt="image"></a>
-		<a href="#"><img src="https://picsum.photos/300/300?image=911" alt="image"></a>
-	</div>
-	<div class="column">
-		<a href="#"><img src="https://picsum.photos/300/300?image=871" alt="image"></a>
-		<a href="#"><img src="https://picsum.photos/300/300?image=872" alt="image"></a>
-		<a href="#"><img src="https://picsum.photos/300/300?image=873" alt="image"></a>
-		<a href="#"><img src="https://picsum.photos/300/300?image=874" alt="image"></a>
-	</div>
-	<div class="column">
-		<a href="#"><img src="https://picsum.photos/300/300?image=861" alt="image"></a>
-		<a href="#"><img src="https://picsum.photos/300/300?image=862" alt="image"></a>
-		<a href="#"><img src="https://picsum.photos/300/300?image=863" alt="image"></a>
-		<a href="#"><img src="https://picsum.photos/300/300?image=864" alt="image"></a>
-	</div>
-	<div class="column">
-		<a href="#"><img src="https://picsum.photos/300/300?image=851" alt="image"></a>
-		<a href="#"><img src="https://picsum.photos/300/300?image=852" alt="image"></a>
-		<a href="#"><img src="https://picsum.photos/300/300?image=853" alt="image"></a>
-		<a href="#"><img src="https://picsum.photos/300/300?image=855" alt="image"></a>
-	</div>
-	<div class="column">
-		<a href="#"><img src="https://picsum.photos/300/300?image=841" alt="image"></a>
-		<a href="#"><img src="https://picsum.photos/300/300?image=842" alt="image"></a>
-		<a href="#"><img src="https://picsum.photos/300/300?image=845" alt="image"></a>
-		<a href="#"><img src="https://picsum.photos/300/300?image=844" alt="image"></a>
-	</div>
-	<div class="column">
-		<a href="#"><img src="https://picsum.photos/300/300?image=831" alt="image"></a>
-		<a href="#"><img src="https://picsum.photos/300/300?image=832" alt="image"></a>
-		<a href="#"><img src="https://picsum.photos/300/300?image=833" alt="image"></a>
-		<a href="#"><img src="https://picsum.photos/300/300?image=834" alt="image"></a>
-	</div>
-</header>
-
-
-
+  <div class="slide">
+    <div v-for="row in slideData" :key="row.id" class="row mb-1">
+      <a v-for="image in row.images" :key="image.id" href="#" class="col p-1">
+        <img :src="image.src" :alt="image.alt" class="slide-image" />
+      </a>
+    </div>
+  </div>
 </template>
 
 
 <style lang="scss" scoped>
-body {
-	min-height: 100vh;
-	display: grid;
-	place-items: center;
+.slide{
+	overflow-x: hidden;
+	background: white;
 }
 
-header {
-	display: flex;
-	overflow: hidden;
-	height: 50vw;
-	background: #111;
+.slide img{
+	opacity: 0.9;
+	filter: saturate(1);
+	transition: all 1s;
+	border-radius: 1rem;
+	aspect-ratio: 1.5/1;
+	object-fit: cover;
 }
 
-header:hover img {
-	opacity: 0.5;
+.slide img:hover {
+	filter: saturate(2);
+	opacity: 1;
 }
 
-header img {
+.slide img {
 	max-width: 100%;
-	vertical-align: middle;
 	border: 1px solid black;
 	box-sizing: border-box;
 	transition: opacity .2s;
 }
 
-header img:hover {
-	opacity: 1;
-}
 
-header .column {
-	animation: var(--animation, none) 16s infinite linear;
-}
-
-header .column:hover {
-	animation-play-state: paused;
-}
-
-header .column:nth-of-type(odd) {
-	align-self: flex-end;
-	--direction: 50%;
-}
-
-@keyframes slide {
-	to { transform: translateY(var(--direction, -50%)) }
-}
 
 
 </style>
