b13a4469f43f11ab09597ceb40c9376cccb6c05b
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-07-06 14:08:26 +0200

    Add NotFound View

--- src/router.js
+++ src/router.js
@@ -1,20 +1,26 @@
 import { createRouter, createWebHistory } from "vue-router";
 import WelcomeView from "./views/WelcomeView.vue";
 import HomeView from "./views/HomeView.vue";
+import NotFound from "./views/NotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    {
+      path: "/:pathMatch(.*)*",
+      name: "404NotFound",
+      component: NotFound,
+    },
     {
       path: "/",
       name: "welcome",
       component: WelcomeView,
     },
     {
-        path: "/home",
-        name: "home",
-        component: HomeView,
-      },
+      path: "/home",
+      name: "home",
+      component: HomeView,
+    },
   ],
 });
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
+
+<template>
+  <section class="notfound">
+    <div class="container py-5">
+      <div class="row">
+        <h1 class="text-center mb-4">Errore 404 - Page not found</h1>
+        <p class="text-center">page doesn't exist</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.notfound .container {
+  min-height: 980px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.image-container {
+  display: flex;
+  justify-content: center;
+}
+
+</style>
