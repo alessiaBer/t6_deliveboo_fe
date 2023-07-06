290249aba7b776fd43786141d418eaa2d48abe3f
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-07-06 20:24:50 +0200

    Add content to menuview

--- src/store.js
+++ src/store.js
@@ -2,17 +2,22 @@ import { reactive } from "vue";
 import axios from "axios";
 
 export const store = reactive({
-    api: "http://127.0.0.1:8000/api/restaurants/",
-    restaurant: null,
-    plates: [],
-    displayMenu(slug) {
-        const url = this.api + slug
-        axios.get(url)
-        .then(response => {
-            console.log(response)
-        })
-        .catch(error => {
-            console.error(error);
-        }); 
-    }
+  api: "http://127.0.0.1:8000/api/restaurants/",
+  restaurant: null,
+  plates: [],
+  displayMenu(slug) {
+    const url = this.api + slug;
+    axios
+      .get(url)
+      .then((response) => {
+        console.log("Response data:", response.data);
+        this.restaurant = response.data.result;
+        this.plates = response.data.result.plates;
+        console.log("Restaurant:", this.restaurant);
+        console.log("Plates:", this.plates);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
 });
--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,22 +1,37 @@
 <script>
-import { RouterLink } from 'vue-router';
-import { store } from '../store';
+import { RouterLink } from "vue-router";
+import { store } from "../store";
+
 export default {
-    name: 'MenuView',
-    components: { RouterLink },
-    data() {
-        return {
-            store
-        }
-    },
-    mounted() {
-        store.displayMenu(this.$route.params.slug)
-    },
-}
+  name: "MenuView",
+  components: { RouterLink },
+
+  data() {
+    return {
+      store,
+    };
+  },
+
+  mounted() {
+    store.displayMenu(this.$route.params.slug);
+  },
+};
 </script>
 <template>
   <div class="container">
+    <template v-if="store.restaurant">
+      <h1>{{ store.restaurant.name }}</h1>
+      <p>{{ store.restaurant.address }}</p>
+      <p>{{ store.restaurant.description }}</p>
 
+      <h2>Plates:</h2>
+      <ul>
+        <li v-for="plate in store.plates" :key="plate.id">
+          <h3>{{ plate.name }}</h3>
+          <p>{{ plate.description }}</p>
+        </li>
+      </ul>
+    </template>
   </div>
 </template>
 
