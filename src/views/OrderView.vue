3f6d0e4deb62ce510765ae7cde876ca2c130babd
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-07-10 15:38:16 +0200

    ADD modified post order by alebert

--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -1,11 +1,14 @@
 <script>
 import { store } from '../store';
-
 export default {
   data() {
     return {
       store
     }
+  },
+  mounted() {
+    store.returnIds()
+    store.calcTotPrice()
   }
 };
 </script>
@@ -16,44 +19,18 @@ export default {
     <form @submit.prevent="store.postOrder()">
       <div class="mb-3">
         <label for="fullname" class="form-label">Full Name</label>
-        <input
-          type="text"
-          class="form-control"
-          name="fullname"
-          id="fullname"
-          v-model="store.fullname"
-          placeholder="John Doe"
-        />
+        <input type="text" class="form-control" name="fullname" id="fullname" v-model="store.fullname"
+          placeholder="John Doe" />
         <label for="fullname" class="form-label">Address</label>
-        <input
-          type="text"
-          class="form-control"
-          name="address"
-          id="address"
-          v-model="store.address"
-          placeholder="Via Parini 6"
-        />
+        <input type="text" class="form-control" name="address" id="address" v-model="store.address"
+          placeholder="Via Parini 6" />
         <label for="phone" class="form-label">Phone</label>
-        <input
-          type="number"
-          class="form-control"
-          name="phone"
-          id="phone"
-          v-model="store.phone"
-          placeholder="+39 347 77 77 777"
-        />
+        <input type="number" class="form-control" name="phone" id="phone" v-model="store.phone"
+          placeholder="+39 347 77 77 777" />
         <div class="mb-3">
-          <label for="email" class="form-label"
-            >Email address</label
-          >
-          <input
-            type="email"
-            class="form-control"
-            id="email"
-            name="email"
-            v-model="store.email"
-            placeholder="name@example.com"
-          />
+          <label for="email" class="form-label">Email address</label>
+          <input type="email" class="form-control" id="email" name="email" v-model="store.email"
+            placeholder="name@example.com" />
         </div>
         <!-- pagamento qui -->
         <button class="btn btn-outline-dark">Confirm</button>
@@ -61,5 +38,4 @@ export default {
     </form>
   </div>
 </template>
-
 <style lang="scss" scoped></style>
