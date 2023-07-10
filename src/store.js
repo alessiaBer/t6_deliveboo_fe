77d8ff368ea16ee9f9d9cfac84a2f83eea8fd7f2
alessiaBer <alexdeyn@gmail.com>
2023-07-10 18:16:38 +0200

    [add] payment method

--- src/store.js
+++ src/store.js
@@ -101,4 +101,38 @@ export const store = reactive({
         console.error(error);
       });
   },
+  braintree() {
+    braintree.dropin.create({
+      // Insert your tokenization key here
+      authorization: 'sandbox_gpqggwy8_kxst6vhrdbqkw2dp',
+      container: '#dropin-container'
+    })
+  },
+ /*  submit(instance) {
+        instance.requestPaymentMethod(function (requestPaymentMethodErr, payload) {
+          // When the user clicks on the 'Submit payment' button this code will send the
+          // encrypted payment information in a variable called a payment method nonce
+          $.ajax({
+            type: 'POST',
+            url: '/checkout',
+            data: {'paymentMethodNonce': payload.nonce}
+          }).done(function(result) {
+            // Tear down the Drop-in UI
+            instance.teardown(function (teardownErr) {
+              if (teardownErr) {
+                console.error('Could not tear down Drop-in UI!');
+              } else {
+                console.info('Drop-in UI has been torn down!');
+              }
+            });
+  
+            if (result.success) {
+              $('#checkout-message').html('<h1>Success</h1><p>Your Drop-in UI is working! Check your <a href="https://sandbox.braintreegateway.com/login">sandbox Control Panel</a> for your test transactions.</p><p>Refresh to try another transaction.</p>');
+            } else {
+              console.log(result);
+              $('#checkout-message').html('<h1>Error</h1><p>Check your console.</p>');
+            }
+          });
+        });
+      } */
 });
