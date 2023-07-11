fa1b022f5124917a4343ce3681602120f04b3979
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-07-11 12:54:00 +0200

    ADD mail

--- src/store.js
+++ src/store.js
@@ -4,6 +4,7 @@ export const store = reactive({
   api: "http://127.0.0.1:8000/api/restaurants/",
   base_api: "http://127.0.0.1:8000/",
   post_api: "http://127.0.0.1:8000/api/orders",
+  mail_api: "http://127.0.0.1:8000/api/leads",
   restaurant: null,
   plates: [],
   cart: [],
@@ -113,31 +114,25 @@ export const store = reactive({
       container: '#dropin-container'
     })
   },
-  /*  submit(instance) {
-         instance.requestPaymentMethod(function (requestPaymentMethodErr, payload) {
-           // When the user clicks on the 'Submit payment' button this code will send the
-           // encrypted payment information in a variable called a payment method nonce
-           $.ajax({
-             type: 'POST',
-             url: '/checkout',
-             data: {'paymentMethodNonce': payload.nonce}
-           }).done(function(result) {
-             // Tear down the Drop-in UI
-             instance.teardown(function (teardownErr) {
-               if (teardownErr) {
-                 console.error('Could not tear down Drop-in UI!');
-               } else {
-                 console.info('Drop-in UI has been torn down!');
-               }
-             });
-   
-             if (result.success) {
-               $('#checkout-message').html('<h1>Success</h1><p>Your Drop-in UI is working! Check your <a href="https://sandbox.braintreegateway.com/login">sandbox Control Panel</a> for your test transactions.</p><p>Refresh to try another transaction.</p>');
-             } else {
-               console.log(result);
-               $('#checkout-message').html('<h1>Error</h1><p>Check your console.</p>');
-             }
-           });
-         });
-       } */
+  postMail() {
+    const data = {
+      clientEmail: this.email,
+      userEmail: this.restaurant.user.email,
+    };
+    axios
+      .post(this.mail_api, data)
+      .then((response) => {
+        if (response.data.success) {
+          console.log("Bravo Palino");
+          this.email = "";
+
+        } else if (response.data.success === false) {
+          console.log(response.data.errors);
+          this.errors = response.data.errors;
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
 });
